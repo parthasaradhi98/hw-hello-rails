a1722de26021b29eb7819553e0a1989f2e95c107
Partha Saradhi <parthasaradhi098@gmail.com>
2022-10-24 14:44:28 -0500

    fourth commit

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,7 +17,17 @@ more_movies = [
   {:title => 'Nomadland', :rating => 'R',
     :release_date => '19-Feb-2021'},
   {:title => 'CODA', :rating => 'PG-13',
-    :release_date => '13-Aug-2021'}
+    :release_date => '13-Aug-2021'},
+  {:title => 'Inception', :rating => 'PG-13',
+    :release_date => '13-July-2010'},
+  {:title => 'Interstellar', :rating => 'PG-13',
+    :release_date => '26-Oct-2014'},
+  {:title => 'Gravity', :rating => 'PG-13',
+    :release_date => '04-Oct-2013'},
+  {:title => 'The Dark Knight', :rating => 'PG-13',
+    :release_date => '18-July-2008'},
+  {:title => 'Dunkirk', :rating => 'PG-13',
+    :release_date => '21-July-2017'}
 ]
 
 more_movies.each do |movie|
